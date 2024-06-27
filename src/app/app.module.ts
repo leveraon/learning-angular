dcf335bc6b3d9e982b26ed9e9b4a06a94a4b1990
Yu Sang <yu.sang@cgi.com>
2024-06-27 13:11:10 -0300

    update angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SafeHtmlPipe } from './pipe/safe-html.pipe';
 import { SharedUiModule } from './shared-ui/shared-ui.module';
-import { booksReducer } from './book-list/state/books.reducer';
-import { collectionReducer } from './book-list/state/collection.reducer';
-import { StoreModule } from '@ngrx/store';
 
-@NgModule({ declarations: [
-        AppComponent,
-        SafeHtmlPipe,
-    ],
-    bootstrap: [AppComponent], imports: [BrowserModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        MatButtonModule,
-        SharedUiModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
-export class AppModule {
-
-}
+@NgModule({
+  declarations: [AppComponent, SafeHtmlPipe],
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    SharedUiModule,
+  ],
+  providers: [provideHttpClient(withInterceptorsFromDi())],
+})
+export class AppModule {}
