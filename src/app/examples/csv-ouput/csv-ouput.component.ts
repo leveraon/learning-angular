88a63a896839ea9804d9aa6965b49a688adac8e4
Yu Sang <yu.sang@cgi.com>
2024-09-13 14:19:06 -0300

    Update example

--- src/app/examples/csv-ouput/csv-ouput.component.ts
+++ src/app/examples/csv-ouput/csv-ouput.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-csv-ouput',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+  ],
   templateUrl: './csv-ouput.component.html',
-  styleUrl: './csv-ouput.component.scss'
+  styleUrl: './csv-ouput.component.scss',
 })
-export class CsvOuputComponent {
+export class CsvOuputComponent implements OnInit, AfterViewInit {
+  dataSource: MatTableDataSource<[]> = new MatTableDataSource();
+  displayedColumns: string[] = [
+    'Date',
+    'Open',
+    'High',
+    'Low',
+    'Adj Close',
+    'Volume',
+  ];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  loadData(): void {
+    Promise.all([fetch('assets/data/aapl.json')]).then(async ([data]) => {
+      const apple = await data.json();
+      this.dataSource = new MatTableDataSource(apple);
+    });
+  }
 }
