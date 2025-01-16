82f80e07cff3f58a1cb885eb246961693299c642
Yu Sang <yu.sang@cgi.com>
2025-01-16 09:48:46 -0400

    add apex charts lib

---
+++ src/app/examples/apex-charts/apex-charts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apex-charts',
+  standalone: true,
+  imports: [],
+  templateUrl: './apex-charts.component.html',
+  styleUrl: './apex-charts.component.scss'
+})
+export class ApexChartsComponent {
+
+}
